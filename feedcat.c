10d187000546a8adee41229be653e16d2388b5d6
Sim Yu Lau <116850311+simyulau@users.noreply.github.com>
2025-12-09 14:00:37 -0500

    read takes in an "entire line" as input
    It will remove 
    1) all trailing spaces
    2) all tailing spaces
    3) 1 character of backslash /  ( but allow you to use / as escape)
    4) the last line (in file) if that line doesn't have newline

---
+++ feedcat.c
@@ -0,0 +1,8 @@
+apple orange banana
+today is tuesday
+           leadingSpace
+trailing space           
+testslash \ 
+testepath /
+testing newline\n
+no newline
---
+++ testing_read.c
@@ -0,0 +1,30 @@
+Script started on 2025-12-09 13:37:40-05:00
+Default printer is: prism
+]0;sylau:/eecs/home/sylaucrimson 501 $ cat weakcat
+#!/bin/bash
+while read myline
+do
+echo $myline
+done < $1
+]0;sylau:/eecs/home/sylaucrimson 502 $ cat feedcat.c
+apple orange banana
+today is tuesday
+           leadingSpace
+trailing space           
+testslash \ 
+testepath /
+testing newline\n
+no newline]0;sylau:/eecs/home/sylaucrimson 503 $ weakcat feedcat.c
+apple orange banana
+today is tuesday
+leadingSpace
+trailing space
+testslash
+testepath /
+testing newlinen
+]0;sylau:/eecs/home/sylaucrimson 504 $ done > test
+testing_read  testsort.c    
+crimson 504 $ done > test[K[K[K[K[K e[K[K[K[K[K[K[K[Kexit
+exit
+
+Script done on 2025-12-09 13:39:21-05:00
